e795cadf4932158f7568e032019d65c7c982a9cb
bland <blando_macmac@yahoo.com>
2021-11-29 16:11:06 +0800

    [Initial Commit for Retrofit&DB Branch]
    - This is the start of the project for Retrofit&DB Branch

---
+++ app/src/main/java/com/rastatech/projectrasta/models/wish/Wish.kt
@@ -0,0 +1,14 @@
+package com.rastatech.projectrasta.models.wish
+
+data class Wish (
+
+    val wish_id : String,
+    val wish_name : String,
+    val description: String,
+    val rastagems_required: Int,
+    val rastagems_donated: Int,
+    val user_id: String,
+
+
+        )
+
